a81c199706f91af08f0210a13e9ee6f0a3dd409b
SaitenHazard <khantowsifkhan@gmail.com>
2021-02-02 22:54:10 +0600

    02/02/2021 10:54PM

--- Nodes/GameManager.gd
+++ Nodes/GameManager.gd
@@ -2,14 +2,14 @@ extends Node
 
 var tile = load("res://Nodes/Tile.tscn");
 export var initial_position : Vector2 = Vector2(144,80)
-export var size : Vector2 = Vector2(15,10)
+export var map_size : Vector2 = Vector2(15,10)
 
 func _ready():
 	_make_map()
 
 func _make_map():
-	for x in range(0, size.x):
-		for y in range (0, size.y):
+	for x in range(0, map_size.x):
+		for y in range (0, map_size.y):
 			var temp = tile.instance()
 			temp.global_position = Vector2(initial_position.x + x*16 , initial_position.y + y*16)
 			if(x == 0 && y == 0):
@@ -18,4 +18,6 @@ func _make_map():
 
 func _get_initial_position():
 	return initial_position
-	
+
+func _get_map_size():
+	return map_size;
--- Nodes/Player.gd
+++ Nodes/Player.gd
@@ -2,21 +2,24 @@ extends Sprite
 
 var coordinates : Vector2 = Vector2(0,0)
 var initial_position : Vector2 = Vector2(0,0)
+var size
 
 func _ready():
 	initial_position = get_parent().get_node("GameManager")._get_initial_position();
+	size = get_parent().get_node("GameManager")._get_map_size();
 	print(initial_position)
 
 func _process(delta):
 	_controls()
+	print(coordinates)
 	global_position = Vector2(initial_position.x + 16*coordinates.x, initial_position.y + 16*coordinates.y)
 
 func _controls():
-	if(Input.is_action_just_released("up")):
+	if(Input.is_action_just_released("up") && coordinates.y > 0):
 		coordinates.y -= 1
-	if(Input.is_action_just_released("down")):
+	if(Input.is_action_just_released("down") && coordinates.y < size.y-1):
 		coordinates.y += 1
-	if(Input.is_action_just_released("left")):
+	if(Input.is_action_just_released("left") && coordinates.x > 0):
 		coordinates.x -= 1	
-	if(Input.is_action_just_released("right")):
+	if(Input.is_action_just_released("right") && coordinates.x < size.x-1):
 		coordinates.x += 1
